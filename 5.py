8061febac1b1ba7ccb93efcf90c6d14a32391b82
pavann449 <111037973+pavann449@users.noreply.github.com>
2025-08-10 19:46:07 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,19 @@
+#slicing strings
+
+
+name="pavannaik"
+print(name[2])  #index= position -1
+print(name[1:5])
+print(name[2:])
+print(name[:7])
+print(name[-6])
+print(name[: :3])# for skip
+
+
+#escape sequence
+# \n = new line
+# \t =tab space
+# \\ = backslash
+
+tab= "pavan \nis good boy"
+print(f"{tab}")
